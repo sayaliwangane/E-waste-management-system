625a3dc002eb7c526a16570eb3a13bb3b4a52cd7
sayaliwangane <75221058+sayaliwangane@users.noreply.github.com>
2021-08-08 20:41:19 +0530

    Add files via upload

---
+++ E-waste calculator/calc.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>E-waste Calculator</title>
+        <link rel="stylesheet" type="text/css" href="stylesheet.css">
+        <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    </head>
+    <body>
+
+        <header>
+            <div class="header-left">
+                <a href="http://localhost/E-waste/E-waste%20calculator/calc.php"><div class="logo"> <img class="lg" src = "green-energy.png">E-waste Calculator</div></a>
+            </div>
+
+            <div class="header-right">
+                <ul>
+                
+                    <li><a href="http://localhost/E-waste/recyclemap.html">Donate</a></li>
+                    <li><a href="http://localhost/E-waste/recyclemap.html">Recycle</a></li> 
+                    <li><a class="ewaste" href="http://localhost/E-waste/index.php">Home</a></li>
+                </ul>
+            </div>
+        </header>
+
+        <!--<div class="wrapper">
+            <h1 class="text1">E-waste Calculator</h1>
+            <p class="ptext1"><b>Would you like to guess<br> how much carbon emission is reduced when you <br>recycle discarded electronic items?</b></p>
+            <div class ="btn-wrapper">
+                <a  href="#S1" class="btn checkout">Check out the E-waste Calculator</a>
+            </div>
+        </div>-->
+
+        <div id="S1" class="steps">
+            <ul class = "flex-list">
+                <li class="li1">Step 1</li>
+                <li class="li2">Step 2</li>
+                <li class="li3">Step 3</li>
+            </ul>
+            <ul class="flex-arrow">
+                <li class="li1"><img class="arr" src= "https://weee4future.eitrawmaterials.eu/wp-content/uploads/2020/09/arrow.svg"></li>
+                <li class="li2"><img class="arr" src= "https://weee4future.eitrawmaterials.eu/wp-content/uploads/2020/09/arrow_light.svg"></li>
+                <li class="li3"><img class="arr" src= "https://weee4future.eitrawmaterials.eu/wp-content/uploads/2020/09/arrow_light.svg"></li>
+            </ul>
+        </div>
+
+        <p class="text2">Have a scavenger hunt at home! <br>Search for any old, broken or neglected gadgets stored somewhere!</p>
+
+        <div class="step1">
+            <div class="text3">Find all those electronics<br> that you don’t use anymore.</div>
+            <div class ="btn-wrapper">
+                <a  href="step2.php" class="btn findings">ENTER YOUR FINDINGS</a>
+            </div>
+        </div>
+
+        <footer>
+        <div class="wrap">
+            <a href="http://localhost/E-waste/index.php"><div class="logo"> <img class="lg" src = "green-energy.png"> E-waste Management System</div></a>
+            <div id="contactus"> <h4> CONTACT US </h4> </div>
+            <div id="info">
+              <p> 500, Mumbai </p>
+              <p>	T: 1300 229 837	</p>
+              <p>	E: enquiries@Ewaste.com.in </p>
+            </div>
+			<div id="endline"> <p> @2021 - Ewaste </p> </div>
+        </div>
+        
+        </footer>
+    </body>
+</html>
+
+<!-- -->
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>E-waste Management System</title>
+        <link rel="stylesheet" type="text/css" href="s.css">
+        <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        
+    </head>
+    <body>
+        <header>
+            <div class="wrap">
+                <div class="header-left">
+                  <a href="index.php"><div class="logo"> <img class="lg" src = "green-energy.png"> E-waste Management System</div></a>
+                </div>
+                <div class="header-right">
+                <ul >
+                    <li><a href="donationmap.html">Donate</a></li>
+                    <li><a href="recyclemap.html">Recycle</a></li> 
+                    <li><a class="ewaste" href="E-waste calculator\calc.php">E-waste Calculator</a></li>
+                </ul>
+            </div>
+            </div>
+        </header>
+        <div class="wrap">
+            <div class="bg-wrap">
+                <img class="im" src="bg.jpg" style="margin:0px">
+            </div>
+                <p class="ptext1">Would you like to guess how much carbon emission is reduced<br> when you recycle discarded electronic items?</p>
+                <div class ="btn-wrapper">
+                    <a  href="E-waste calculator/calc.php" class="btn checkout">Check out the E-waste Calculator</a>
+                </div>
+                
+                <p class="ptext2">Would you like to Donate or Recycle your electronics ?</p>
+                
+                <div class ="btn-wrapper">
+                    <a  href="donationmap.html" class="btn donate">Donate</a>
+                </div>
+                <div class ="btn-wrapper">
+                    <a  href="recyclemap.html" class="btn recycle">Recycle</a>
+                </div>
+
+        </div>
+        
+          
+    <footer>
+        <div class="wrap">
+        <a href="index.php"><div class="logo"> <img class="lg" src = "green-energy.png"> E-waste Management System</div></a>
+            <div id="contactus"> <h4> CONTACT US </h4> </div>
+            <div id="info">
+              <p> 500, Mumbai </p>
+              <p>	T: 1300 229 837	</p>
+              <p>	E: enquiries@Ewaste.com.in </p>
+            </div>
+			<div id="endline"> <p> @2021 - Ewaste </p> </div>
+        </div>
+        
+        </footer>
+    </body>
+</html>
